3a90050fd01e663aa59ee68702cb9e1f77ddec50
arifur rahman <arifurrex@gmail.com>
2022-12-27 21:55:54 +0600

    this is update

--- fetch.js
+++ fetch.js
@@ -15,14 +15,42 @@
 
 
 //! post 
-fetch('https://jsonplaceholder.typicode.com/posts',{
-    method: 'POST',
+// fetch('https://jsonplaceholder.typicode.com/posts',{
+//     method: 'POST',
+//     headers: {
+//         'Content-type': 'application/json; charset=UTF-8',
+//       },
+//     body:JSON.stringify({
+//         title: 'fatra',
+//         body:'batra',
+//         userId:1,
+//     })
+// })
+// .then(function(res){
+//     if(!res.ok){
+//       const massage = `Error : ${res.status}`;
+//       throw new Error (massage)
+//     }
+//     return res.json();
+// })
+// .then(function(res){
+//     console.log(res);
+// })
+// .catch(function(err){
+//     console.log(err);
+// })
+ 
+
+//! update
+fetch('https://jsonplaceholder.typicode.com/posts/1',{
+    method: 'PUT',
     headers: {
         'Content-type': 'application/json; charset=UTF-8',
       },
     body:JSON.stringify({
-        title: 'fatra',
-        body:'batra',
+        id:1,
+        title: 'fatrabora',
+        body:'batrawata',
         userId:1,
     })
 })
@@ -39,3 +67,12 @@ fetch('https://jsonplaceholder.typicode.com/posts',{
 .catch(function(err){
     console.log(err);
 })
+
+
+
+
+
+
+
+
+
