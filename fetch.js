889fe0d0aa3a516149d9fa22f3d9ac4f91c6199d
arifur rahman <arifurrex@gmail.com>
2022-12-27 21:52:48 +0600

    post done

--- fetch.js
+++ fetch.js
@@ -1,4 +1,31 @@
-fetch('https://jsonplaceholder.typicode.com/post')
+// fetch('https://jsonplaceholder.typicode.com/post')
+// .then(function(res){
+//     if(!res.ok){
+//       const massage = `Error : ${res.status}`;
+//       throw new Error (massage)
+//     }
+//     return res.json();
+// })
+// .then(function(res){
+//     console.log(res);
+// })
+// .catch(function(err){
+//     console.log(err);
+// })
+
+
+//! post 
+fetch('https://jsonplaceholder.typicode.com/posts',{
+    method: 'POST',
+    headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+      },
+    body:JSON.stringify({
+        title: 'fatra',
+        body:'batra',
+        userId:1,
+    })
+})
 .then(function(res){
     if(!res.ok){
       const massage = `Error : ${res.status}`;
